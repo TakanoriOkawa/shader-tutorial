3d9464bcf32e060d159c903d821b383415f184e9
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-22 02:04:41 +0900

    説明追加

--- src/shader/fragmentShader.glsl
+++ src/shader/fragmentShader.glsl
@@ -1,5 +1,7 @@
-precision mediump float;
+precision mediump float; // Rawだと宣言が必要
+// データの精度を決める
 
 void main(){
-  gl_FragColor = vec4(1.0, 0, 0, 1.0);
+  // 色を塗る
+  gl_FragColor = vec4(1.0, 1, 0, 1.0); // rgbaを表す
 }
--- src/shader/vertexShader.glsl
+++ src/shader/vertexShader.glsl
@@ -1,7 +1,7 @@
-// 定義の順番が大事
-uniform mat4 projectionMatrix; // 命名は決まっている
-uniform mat4 viewMatrix; // 
-uniform mat4 modelMatrix; //
+// 定義の順番が大事、命名は決まっている
+uniform mat4 projectionMatrix; // カメラがどこまで写すんのかを決める行列
+uniform mat4 viewMatrix; // 写すカメラがどこに位置しているのかを決める行列
+uniform mat4 modelMatrix; // どこにあるかの位置情報決める行列
 
 attribute vec3 position;
 
@@ -9,4 +9,7 @@ void main(){
   // gl_Positionはglslで決まったものなのでスペルミスしないように
   gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
   // セミコロンがないとエラーになる
+
+  // 3つの行列を掛け合わせて（行列の掛け算）
+  // 3D空間を2Dに座標変換している
 }
